aa0b074ae326a87480a75f4b4c5c1f12a81cdfb2
Tem <temurbakhriddinov@gmail.com>
2024-08-01 18:58:01 -0500

    add indicators

--- src/app/(public)/page.tsx
+++ src/app/(public)/page.tsx
@@ -1,5 +1,13 @@
+import Link from 'next/link';
+
 const Page = () => {
-	return <div>Page</div>;
+	return (
+		<div className='flex h-full w-full items-center justify-center'>
+			<Link href='/feature1' className='underline underline-offset-2'>
+				Go to user (signed in) routes
+			</Link>
+		</div>
+	);
 };
 
 export default Page;
